c3f96072edbd60650b4b51e104380e881e8f1ced
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-08-06 01:01:35 +0900

    chore: router 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const elem = document.getElementById('root');
 if (!elem) throw new Error("root element is not exist");
 const root = ReactDOM.createRoot(elem);
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        {/* <Route path="auth" element={} /> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
