0b8c1bfc48c272532f1f9a1702acac4427978350
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-08-06 00:11:23 +0900

    chore: typescript 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const elem = document.getElementById('root');
+if (!elem) throw new Error("root element is not exist");
+const root = ReactDOM.createRoot(elem);
+
 root.render(
   <React.StrictMode>
     <App />
