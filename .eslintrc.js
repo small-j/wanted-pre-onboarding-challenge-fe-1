26617befa5f01883d87fbc16a800558d95f33255
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-08-06 00:47:00 +0900

    chore: eslint & prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+    ],
+    extends: [
+      "plugin:prettier/recommended",
+      'plugin:@typescript-eslint/recommended',
+    ],
+  };
