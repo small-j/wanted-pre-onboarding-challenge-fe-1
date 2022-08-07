e89f7b9d04a86cb14b44677c07f50e14d915bbda
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-08-07 18:43:15 +0900

    chore: theme 설정 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import CustomRouter from './CustomRouter';
 import { GlobalStyle } from './assets/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './assets/theme';
 
 const elem = document.getElementById('root');
 if (!elem) throw new Error("root element is not exist");
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(elem);
 
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <CustomRouter />
+    </ThemeProvider>
   </React.StrictMode>
 );
